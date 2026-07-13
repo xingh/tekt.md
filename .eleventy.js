50f8e345e86ba9def42d04e94c096996abeb2d94
Rahul Singh <rahul.singh@anant.us>
2026-07-13 19:24:58 -0400

    Add passthrough copy for body and cover images

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("install.sh");
   eleventyConfig.addPassthroughCopy("install.ps1");
   eleventyConfig.addPassthroughCopy("tekt.catalog.yaml");
+  eleventyConfig.addPassthroughCopy("tekt.body.png");
+  eleventyConfig.addPassthroughCopy("tekt.cover.png");
   return {
     dir: {
       input: ".",
